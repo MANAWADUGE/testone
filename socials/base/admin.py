57278e91184414c0bcb8f27b15593c5a4a91bb06
rwi_Manawaduge <81893061+MANAWADUGE@users.noreply.github.com>
2023-08-14 11:22:38 +0530

    Add files via upload

---
+++ socials/base/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(FollowersCount)
+admin.site.register(Comment)
+admin.site.register(LikePost)
+
+
+
